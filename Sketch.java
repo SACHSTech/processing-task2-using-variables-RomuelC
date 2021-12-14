2e5696b7dc65c2d5b33ef591c6878e7aec37c7f0
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2021-12-13 21:06:10 -0500

    test

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
   /** 
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(122, 163, 240);
   }
 
   /**
@@ -25,11 +25,38 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    stroke(60);
+    
+    // Grass
+    fill(31, 120, 13);
+    rect(0, 400, 500, 100);
+    
+    // Church
+    fill(148, 62, 9);
+    rect(200, 300, 200, 300);
+    
+    // Church Door
+    fill(15, 15, 15);
+    rect(275, 450, 50, 70);  
+    
+    // Roof
+    fill(123, 135, 120);
+    triangle(300, 10, 200, 300, 400, 300);
+    
+    // Cross Y
+    line(300, 120, 300, 200);  
+    
+    // Cross X
+    line(330, 150, 270, 150);    
+    
+    // Left Window
+    fill(119, 64, 237);
+    ellipse(350, 400, 45, 45);
+    
+    // Right Window
+    fill(119, 64, 237);
+    ellipse(250, 400, 45, 45);
+    
   }
   
   // define other methods down here.
